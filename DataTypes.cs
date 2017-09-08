82aabf910e3d064ef6d262609672bfbff3f5d4ff
Dino Lozina <dino.lozina@live.com>
2017-09-08 10:19:47 +0200

    Add files via upload

---
+++ DataTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koncar_Siemens_WPF
+{
+    public struct AreaPoint
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct VelocityPoint
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct CameraOutput
+    {
+        public AreaPoint POINT1;
+        public AreaPoint POINT2;
+        public float PARAMETER;
+        public short TYPE;
+    }
+
+}
